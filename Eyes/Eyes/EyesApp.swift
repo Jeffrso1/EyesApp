33f7b2f7f274b0bf06f32b801f5f1927bd7ba73d
Lucas Frazao <lucasnfrazao@icloud.com>
2020-10-19 16:47:10 -0300

    Initial Xcode Configuration :tada:

---
+++ Eyes/Eyes/EyesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EyesApp.swift
+//  Eyes
+//
+//  Created by Lucas Frazão on 19/10/20.
+//
+
+import SwiftUI
+
+@main
+struct EyesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
