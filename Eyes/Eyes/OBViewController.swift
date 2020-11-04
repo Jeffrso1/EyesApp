642458ee57d46fbbcda84fe4d668574bc6a6317b
Jefferson Silva <jefferson9334@gmail.com>
2020-11-04 17:26:53 -0300

    OnBoarding ready to go!

---
+++ Eyes/Eyes/OBViewController.swift
@@ -0,0 +1,17 @@
+//
+//  OBViewController.swift
+//  Eyes
+//
+//  Created by Jefferson Silva on 04/11/20.
+//
+
+import UIKit
+
+class OBViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
